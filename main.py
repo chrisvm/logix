c97230a510fe77360abb2b80246379eb5af427c8
Christian Velez <christian.velez5@upr.edu>
2014-10-07 23:26:30 -0400

    - Add comments to code - Changed some variable names to be more expressive

--- main.py
+++ main.py
@@ -3,24 +3,30 @@ import evaluate
 import pprint
 import sys
 from parser import Parser
-#TODO: Fix EOF error
-#TODO: add readline support
 
 
 def main():
 	parser = Parser()
-
 	while True:
+		# Get input 
 		expr = input("> ")
+		
+		# If input 'quit' or 'q', exit the program
 		if expr.lower() in {'quit', 'q'}:
 			sys.exit()
+
+		# Create the ast 
 		ast = tree.create(parser.parse(expr))
 
-		d = evaluate.find_vars(ast)
+		# Get the variables used in the expression 
+		# and get a printable repr of them 
+		expression_vars = evaluate.find_vars(ast)
 		variables = ''
-		for v in d:
-			variables += v
+		for var in expression_vars:
+			variables += var
 
+		# Print the variables used and the results
+		# from the truth table 
 		print('vars: %s' % str(variables))
 		results = evaluate.eval(ast, variables)
 		pprint.pprint(results)
